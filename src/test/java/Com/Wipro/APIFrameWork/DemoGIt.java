54a1c519f3be4b5749cea44e250a4e6172e8e863
Venkat <venkateshangara666@gmail.com>
2024-07-29 18:08:16 +0530

    Commit from X person From India

---
+++ src/test/java/Com/Wipro/APIFrameWork/DemoGIt.java
@@ -0,0 +1,11 @@
+package Com.Wipro.APIFrameWork;
+
+public class DemoGIt {
+	public static void main(String[] args) {
+		System.out.println("Demo purpose");
+		System.out.println("Git Repo");
+		System.out.println("Indain Employee");
+		
+	}
+
+}
